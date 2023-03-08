5a649bf96bcdf198d9b38471a9e18bae27957312
raphael-foliveira <raphael.foliveira1@gmail.com>
2023-03-08 02:21:01 -0300

    init

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Expense Tracker Ecopel')
+    .setTitle('Expense Tracker')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
